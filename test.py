c4ebf412317e6cacd7bd107c4f825acd84394437
AdivarekarBhumit <bhumit97ad@gmail.com>
2018-10-06 02:27:50 +0530

    Abc

--- test.py
+++ test.py
@@ -3,7 +3,32 @@ import telegram
 import copy
 import logging
 
+class Users:
+    user_dict = {}
+    def __init__(self, user_id):
+        self.user_dict['user_id'] = user_id
+
+    def add_name(self, name):
+        self.user_dict['name'] = user_id
+    
+    def add_latlng(self, latlng):
+        self.user_dict['latlng'] = latlng
+    
+    def add_file_info(self, file_info):
+        self.user_dict['file_info'] = file_info
+
+    def add_contact(self, phone_no):
+        self.user_dict['contact'] = phone_no
+
+    def add_description(self, desc):
+        self.user_dict['desc'] = desc
+        
+    def get_user_id(self):
+        return self.user_dict['user_id']
+
+
 content = {}
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -15,26 +40,28 @@ def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 def get_info(update):
+    
     upd = copy.deepcopy(update.to_dict())
     #print(update)
     user_id = update.message.from_user.id
-    content['user_id'] = user_id
+    
+    #content['user_id'] = user_id
     print(user_id)
     first_name = update.message.from_user.first_name
     last_name = update.message.from_user.last_name
-    content['first_name'] = first_name
-    content['last_name'] = last_name
+    #content['first_name'] = first_name
+    #content['last_name'] = last_name
     msg = update.message.text
     print(msg)
     if upd.get('message').get('location') != None:
         lat = upd.get('message').get('location').get('latitude')
         lng = upd.get('message').get('location').get('longitude')
         print(str(lat) + ',' + str(lng))
-        content['latlng'] = str(lat) + ',' + str(lng)
+        #content['latlng'] = str(lat) + ',' + str(lng)
     
     if update.message.photo != []:
         print(update.message.photo[-1].get_file()['file_path'])
-        content['file_info'] = update.message.photo[-1].get_file()['file_path']
+        #content['file_info'] = update.message.photo[-1].get_file()['file_path']
 
 def echo(bot, update):
     if update.message.photo != []:
@@ -53,13 +80,14 @@ def location(bot, update):
 def contact(bot, update):
     upd = update.to_dict()
     phone_no = str('+') + upd.get('message').get('contact').get('phone_number')
-    content['contact'] = phone_no
+    #content['contact'] = phone_no
     print(phone_no)
     
 
 def description(bot, update):
     desc = update.to_dict().get('message').get('text')
     content['description'] = desc
+    bot.send_message(chat_id=update.message.chat_id, text='Your Complaint has been lodged.')
     print(content)
     
 def main():
@@ -83,4 +111,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    content = {}
